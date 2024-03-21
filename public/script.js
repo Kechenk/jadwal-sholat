4c562f088fac9340430822a1a77684402a6132d4
kechenk <rezhakechenk67@gmail.com>
2024-03-21 18:22:04 +0700

    updated

--- public/script.js
+++ public/script.js
@@ -1,11 +1,97 @@
-$('.town-select').select2();
+function dropdownProvinsi() {
+  fetch("https://kechenk.github.io/api-wilayah-indonesia/api/provinces.json")
+    .then((response) => response.json())
+    .then((data) => {
+      var pilihProvinsi = document.getElementById("pilihProvinsi");
+      data.forEach((provinces) => {
+        var option = document.createElement("option");
+        option.value = provinces.id;
+        option.textContent = provinces.name;
+        pilihProvinsi.appendChild(option);
+      });
+    })
+    .catch((error) => console.error("Error fetching provinces", error));
+}
 
-        $('.town-select').change(function () {
-            const currParam = new URLSearchParams(window.location.search);
-            const currLocation = window.location.href.split('?')[0];
+function dropdownKota() {
+  var idProvinsi = document.getElementById("pilihProvinsi").value;
+  fetch(
+    `https://kechenk.github.io/api-wilayah-indonesia/api/regencies/${idProvinsi}.json`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      var pilihKota = document.getElementById("pilihKota");
+      pilihKota.innerHTML = "";
+      data.forEach((city) => {
+        var option = document.createElement("option");
+        option.value = city.id;
+        option.textContent = city.name;
+        pilihKota.appendChild(option);
+      });
+      pilihKota.dispatchEvent(new Event("change"));
+    });
+  console.log(idProvinsi);
+}
 
-            currParam.set('id', this.value);
-            localStorage.setItem('location_id', this.value);
+function jamSholat() {
+  var pilihKota = document.getElementById("pilihKota");
+  var namaKota = pilihKota.options[pilihKota.selectedIndex].text;
+  var today = new Date();
+  var year = today.getFullYear();
+  var month = today.getMonth() + 1;
+  var monthNames = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+  var api = `https://api.aladhan.com/v1/calendarByCity/${year}/${month}?city=${namaKota}&country=Indonesia&method=20`;
+  fetch(api)
+    .then((response) => response.json())
+    .then((data) => {
+      var waktuSholat = data.data;
+      var monthYear = `${monthNames[month - 1]} - ${year}`;
+      document.getElementById("city").innerText = namaKota;
+      document.getElementById("month_year").innerText = monthYear;
+      var table = document.querySelector(".data_sholat");
+      table.innerHTML = "";
+      displayWaktuSholat(waktuSholat);
+      displayTime();
+    });
+  //console.log
+}
 
-            window.location.href = `${currLocation}?${currParam.toString()}`;
-        });
+function displayWaktuSholat(waktuSholat) {
+    var table = document.querySelector(".data_sholat");
+    table.innerHTML = "";
+    var today = new Date().getDate();
+    waktuSholat.forEach((day, index) => {
+      var row = document.createElement("tr");
+      var className = index % 2 === 0 ? "table_dark" : "table_light"; 
+      if (index + 1 === today) { 
+        className = "table_highlight"; 
+      }
+      row.className = "table_row " + className; 
+      row.innerHTML = `
+          <td>${day.date.gregorian.day} / ${day.date.hijri.day} </td>
+          <td>${day.timings.Imsak}</td>
+          <td><b>${day.timings.Fajr}</b></td>
+          <td>${day.timings.Sunrise}</td>
+          <td>${day.timings.Dhuhr}</td>
+          <td>${day.timings.Asr}</td>
+          <td><b>${day.timings.Maghrib}</b></td>
+          <td>${day.timings.Isha}</td>
+      `;
+      table.appendChild(row);
+    });
+  }
+
+dropdownProvinsi();
